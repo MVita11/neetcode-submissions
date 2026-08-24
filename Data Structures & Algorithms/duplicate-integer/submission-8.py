5281bdf8a1049c77482854e56575ab53aa564f1a
Manasse Vita <157376426+MVita11@users.noreply.github.com>
2026-08-24 12:39:09 +0100

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num] = 1
+                continue
+            else:
+                return True
+        return False
+        
